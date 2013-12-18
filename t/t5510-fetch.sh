316d20130a43dd06840052535ac4104093b9b7a5
Tom Miller <jackerran@gmail.com>
2013-12-18 13:44:32 -0600

    fetch --prune: Run prune before fetching
    When we have a remote-tracking branch named "frotz/nitfol" from a
    previous fetch, and the upstream now has a branch named "frotz". Prior
    to this patch fetch would fail to remove "frotz/nitfol" with a "git
    fetch --prune" from the upstream. git would inform the user to use "git
    remote prune" to fix the problem.
    
    This patch changes the way "fetch --prune" works by moving the pruning
    operation before the fetching operation. Instead of warning the user of
    a conflict, it autmatically fixes it.
    
    Signed-off-by: Tom Miller <jackerran@gmail.com>
    Tested-by: Thomas Rast <tr@thomasrast.ch>

--- t/t5510-fetch.sh
+++ t/t5510-fetch.sh
@@ -614,4 +614,18 @@ test_expect_success 'all boundary commits are excluded' '
 	test_bundle_object_count .git/objects/pack/pack-${pack##pack	}.pack 3
 '
 
+test_expect_success 'branchname D/F conflict resolved by --prune' '
+	git branch dir/file &&
+	git clone . prune-df-conflict &&
+	git branch -D dir/file &&
+	git branch dir &&
+	(
+		cd prune-df-conflict &&
+		git fetch --prune &&
+		git rev-parse origin/dir >../actual
+	) &&
+	git rev-parse dir >expect &&
+	test_cmp expect actual
+'
+
 test_done
